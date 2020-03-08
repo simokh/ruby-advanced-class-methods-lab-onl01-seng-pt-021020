ab19b848250a512001fbae2448e0664779153690
Simo Khouili <m.simokhouili@gmail.com>
2020-03-08 16:47:06 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -34,9 +34,9 @@ class Song
   end
 
   def self.find_or_create_by_name(name)
-    product = self.find_by_name(name)
-    if product
-        product
+    song_name = self.find_by_name(name)
+    if song_name
+        song_name
     else song_name = self.create_by_name(name)
     end
   end
